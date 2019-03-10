df6cec42277c430e6447dbb3f07059b789e8c794
Neil Balch <neilbalch@gmail.com>
2019-03-09 23:12:40 -0800

    Add SD Logging

--- include/config.h
+++ include/config.h
@@ -1,13 +1,13 @@
-#undef LED_BUILTIN
-#define LED_BUILTIN PC13
-
-#define LOOP_SPEED 200  // x cycles/sec
-#define DEBUG true      // Print serial messages?
-// TODO(Neil): Which are we going to use? Must be analog!!
-#define THROTTLE_PIN PB5  // Pin associated with the throttle
+#define LOOP_SPEED 200    // x cycles/sec
+#define DEBUG true        // Print serial messages?
+#define SD_CS_PIN PA4     // Pin associated with the SD Card CS line
+#define THROTTLE_PIN PA0  // Pin associated with the throttle
+#define THROTTLE_LOW 1.3  // LOW Voltage from the throttle (Units: V)
+#define THROTTLE_HIGH 5   // HIGH Voltage from the throttle (Units: V)
 // TODO(Neil): Tune these parameters
-#define THROTTLE_CUTOFF 1.0  // Cutoff voltage for enabling the motor
-#define WHEEL_RADIUS 0.5     // Units: meters
-#define MAX_SPEED 8.94       // Units: m/s
+#define THROTTLE_CUTOFF \
+  THROTTLE_LOW + 0.15     // Cutoff voltage for enabling the motor
+#define WHEEL_RADIUS 0.5  // Units: meters
+#define MAX_SPEED 8.94    // Units: m/s
 // rot/min = m/s * 1rot/(2*PI*r)m * 60s/1min
 #define MAX_RPM MAX_SPEED * 60 / (2 * PI * WHEEL_RADIUS)
--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,20 @@
 // 6DOF Accelerometer/Gyro
 MPU6050 mpu6050(Wire);
 
+// Path to currently in-use log file
+String logFile = "";
+
 // Log the debug message only if the DEBUG flag is set
 inline void logMsg(String msg) {
   if (DEBUG) Serial.println("DEBUG: " + msg);
 }
 inline void logErr(String msg) { Serial.println("Error: " + msg); }
+
+// Helper to map floating point values, map() only maps integers
+inline float mapFloat(float x, float in_min, float in_max, float out_min,
+                      float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 struct State {
   // Powered Time
@@ -34,24 +43,70 @@ struct State {
   float targetRPM;
   float targetW;          // Units: rad/s
   float throttleVoltage;  // Units: V
+
+  // Returns a formatted CSV line of the struct contents
+  inline String generateLogLine() {
+    return (String)time + ", " + (String)accX + ", " + (String)accY + ", " +
+           (String)accZ + ", " + (String)gyroX + ", " + (String)gyroY + ", " +
+           (String)gyroZ + ", " + (String)motorPower + ", " +
+           (String)inputPower + ", " + (String)motorVoltage + ", " +
+           (String)inputVoltage + ", " + (String)motorCurrent + ", " +
+           (String)inputCurrent + ", " + (enabled ? "true" : "false") + ", " +
+           (String)targetRPM + ", " + (String)targetW + ", " +
+           (String)throttleVoltage;
+  }
 };
 State state;
 
 void setup() {
+  // Init Serial
   Serial.begin(250000);
+
+  // Init I2C and MPU6050
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(DEBUG);
 
+  // Init throttle sense pin
   pinMode(THROTTLE_PIN, INPUT);
 
-  // TODO(Neil): Open file io
+  // Init SD file IO
+  logMsg("\nSD card init...");
+  if (!SD.begin(SD_CS_PIN)) {
+    logErr("SD Card failed, or not present");
+    while (1) {
+    }
+  }
+  logMsg("SD card initialized");
+
+  // Find out what the latest log file is, and use the next one.
+  int num = 0;
+  while (logFile == "") {
+    if (SD.exists("log" + (String)num + ".csv"))
+      num++;
+    else
+      logFile = "log" + (String)num + ".csv";
+  }
+  logMsg("Using log file: " + (String)logFile);
+
+  // Send header line
+  File log = SD.open(logFile, FILE_WRITE);
+  if (log) {
+    log.println(
+        "time, accX, accY, accZ, gyroX, gyroY, gyroZ, motorPower, inputPower, "
+        "motorVoltage, inputVoltage, motorCurrent, inputCurrent, enabled, "
+        "targetRPM, targetW, throttleVoltage");
+    log.close();
+    logMsg("Header Line Written");
+  } else
+    logErr("Error opening " + (String)logFile);
 }
 
 void loop() {
   state.time = millis();
 
-  {  // Populate MPU6050's state fields
+  {  // Populate MPU6050 state fields
+    mpu6050.update();
     state.accX = mpu6050.getAccX() * 9.80;
     state.accY = mpu6050.getAccY() * 9.80;
     state.accZ = mpu6050.getAccZ() * 9.80;
@@ -62,33 +117,49 @@ void loop() {
   }
 
   // TODO(Neil): Implement VESC Telemetry Recording Here
+  {  // Populate VESC state fields
+    state.motorPower = 0;
+    state.inputPower = 0;
+    state.motorVoltage = 0;
+    state.inputVoltage = 0;
+    state.motorCurrent = 0;
+    state.inputCurrent = 0;
+  }
 
   // Read throttle voltage
   float throttle =
       analogRead(THROTTLE_PIN) * (5.0 /* V*/ / 4095.0 /* steps */);  // Units: V
-  logMsg("Throttle Voltage: " + (String)throttle);
+  // logMsg("Throttle Voltage: " + (String)throttle);
   state.throttleVoltage = throttle;
 
   // Are we enabled?
   if (throttle < THROTTLE_CUTOFF) {  // Disabled
-    logMsg("Disabled");
+    // logMsg("Disabled");
     state.enabled = false;
     state.targetRPM = 0.0;
     state.targetW = 0.0;
   } else {  // Enabled
-    logMsg("Enabled");
+    // logMsg("Enabled");
     state.enabled = true;
 
     // Compute the desired throttle setting
-    float rpm = map(throttle, 0, 5, 0, MAX_RPM);
+    float rpm = mapFloat(throttle, THROTTLE_LOW, THROTTLE_HIGH, 0, MAX_RPM);
     constrain(rpm, 0, MAX_RPM);  // Should be extranous, but just make sure
     state.targetRPM = rpm;
     state.targetW = rpm * 2 * PI / 60;
 
     // TODO(Neil): Send target RPM to ESC
-
-    // TODO(Neil): Write info to CSV
   }
+
+  // Write state info to SD file, log to serial
+  File log = SD.open(logFile, FILE_WRITE);
+  if (log) {
+    String msg = state.generateLogLine();
+    logMsg(msg);
+    log.println(msg);
+    log.close();
+  } else
+    logErr("Error opening " + (String)logFile);
 
   // Generate the loop time delay
   delay(1000 / LOOP_SPEED);
