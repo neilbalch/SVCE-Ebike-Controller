fc1176d4f6244f4166ac23170f7aaf5746fa1151
Neil Balch <neilbalch@gmail.com>
2019-03-29 21:37:00 -0700

    Refactor a bit more

--- include/helpers.h
+++ include/helpers.h
@@ -12,3 +12,37 @@ inline float mapFloat(float x, float in_min, float in_max, float out_min,
                       float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+// Structure to hold sensor values and goals
+struct State {
+  // Controller Powered On-time
+  unsigned long time;  // Units: ms, overflows in ~9hrs
+
+  // Motion Data (from MPU6050)
+  float accX, accY, accZ;     // Units: m/s^2
+  float gyroX, gyroY, gyroZ;  // Units: rad/s
+
+  // Power Consumption (from VESC)
+  long rpm;
+  float motorPower, inputPower;      // Units: W
+  float motorVoltage, inputVoltage;  // Units: V
+  float motorCurrent, inputCurrent;  // Units: A
+
+  // Goals (Generated)
+  bool enabled;
+  float targetRPM;
+  float targetW;          // Units: rad/s
+  float throttleVoltage;  // Units: V
+
+  // Returns a formatted CSV line of the struct contents
+  inline String generateLogLine() {
+    return (String)time + ", " + (String)accX + ", " + (String)accY + ", " +
+           (String)accZ + ", " + (String)gyroX + ", " + (String)gyroY + ", " +
+           (String)gyroZ + ", " + (String)rpm + ", " + (String)motorPower +
+           ", " + (String)inputPower + ", " + (String)motorVoltage + ", " +
+           (String)inputVoltage + ", " + (String)motorCurrent + ", " +
+           (String)inputCurrent + ", " + (enabled ? "true" : "false") + ", " +
+           (String)targetRPM + ", " + (String)targetW + ", " +
+           (String)throttleVoltage;
+  }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,7 @@
 #include <VescUart.h>        // For VESC
 #include <Wire.h>            // For MPU6050 (I2C)
 #include "config.h"          // Constants
-#include "helpers.h"         // Helper Functions
-
-// Structure to hold sensor values and goals
-struct State {
-  // Controller Powered On-time
-  unsigned long time;  // Units: ms, overflows in ~9hrs
-
-  // Motion Data (from MPU6050)
-  float accX, accY, accZ;     // Units: m/s^2
-  float gyroX, gyroY, gyroZ;  // Units: rad/s
-
-  // Power Consumption (from VESC)
-  long rpm;
-  float motorPower, inputPower;      // Units: W
-  float motorVoltage, inputVoltage;  // Units: V
-  float motorCurrent, inputCurrent;  // Units: A
-
-  // Goals (Generated)
-  bool enabled;
-  float targetRPM;
-  float targetW;          // Units: rad/s
-  float throttleVoltage;  // Units: V
-
-  // Returns a formatted CSV line of the struct contents
-  inline String generateLogLine() {
-    return (String)time + ", " + (String)accX + ", " + (String)accY + ", " +
-           (String)accZ + ", " + (String)gyroX + ", " + (String)gyroY + ", " +
-           (String)gyroZ + ", " + (String)rpm + ", " + (String)motorPower +
-           ", " + (String)inputPower + ", " + (String)motorVoltage + ", " +
-           (String)inputVoltage + ", " + (String)motorCurrent + ", " +
-           (String)inputCurrent + ", " + (enabled ? "true" : "false") + ", " +
-           (String)targetRPM + ", " + (String)targetW + ", " +
-           (String)throttleVoltage;
-  }
-};
+#include "helpers.h"         // Helper Objects
 
 // Global objects
 MPU6050 mpu6050(Wire);  // 6DOF Accelerometer/Gyro
